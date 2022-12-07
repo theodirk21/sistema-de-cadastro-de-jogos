0b0ffebfbb5dc0d2c73432bffad7590d27715473
theodirk21 <theodirk17@gmail.com>
2022-12-07 11:40:06 -0300

    teste de envio

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 
 console.log("Fazendo modificação para ver no commit")
+console.log("Enviar para o github")
